b2cef628f69d75d7d0aef7ae34f6be3b180ff3ce
ssoso27 <sohi8290@gmail.com>
2019-07-18 20:30:27 +0900

    day04 과제

--- src/sohi/TempTest.java
+++ src/sohi/TempTest.java
@@ -1,28 +1,23 @@
 package sohi;
 
-import java.util.Scanner;
-
-public class TempTest {
-
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-
-        String p=sc.nextLine();
-        String first = p.substring(0,1);
-
-        for(int a = 1; a <= p.length() / 2; a++){
-            int b = a + a - 1; // 단어의 끝 index
-            String target = p.substring(a, a+1);
-
-            if(first.equals(target)) { // 첫글자랑 같으면
-//                String pattern = p.substring(0, a+1);
-                String pattern = p.substring(0, a);
-                String nextWord = p.substring(a, b+1);
-
-                if(pattern.equals(nextWord)){
-                    System.out.println(a);
+class ASAS {
+    static int indexOf(char[] text, char[] search){
+        if( search.length == 0 ) return 0;
+        for (int i = 0; i < text.length; i++){
+            int count = 0;
+            if (text[i] == search[0]){
+                for (int j = 0; j < search.length; j++){
+                    if (text[i + j] == search[j])
+                        count++;
                 }
+                if (count == search.length)
+                    return i;
             }
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ASAS.indexOf("HE".toCharArray(), "asdfasdf".toCharArray()));
     }
 }
