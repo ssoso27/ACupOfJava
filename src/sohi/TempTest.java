f401d9d681734bd0b755f2e136e2354b6d26974d
ssoso27 <sohi8290@gmail.com>
2019-07-18 17:33:33 +0900

    day04

--- src/sohi/TempTest.java
+++ src/sohi/TempTest.java
@@ -1,13 +1,28 @@
 package sohi;
 
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class TempTest {
-    public static void main(String[] args) {
-        HashMap<String, Integer> runner = new HashMap<>();
-        runner.put("aa", 22);
 
-        int count = runner.get("ab");
-        System.out.println(count);
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        String p=sc.nextLine();
+        String first = p.substring(0,1);
+
+        for(int a = 1; a <= p.length() / 2; a++){
+            int b = a + a - 1; // 단어의 끝 index
+            String target = p.substring(a, a+1);
+
+            if(first.equals(target)) { // 첫글자랑 같으면
+//                String pattern = p.substring(0, a+1);
+                String pattern = p.substring(0, a);
+                String nextWord = p.substring(a, b+1);
+
+                if(pattern.equals(nextWord)){
+                    System.out.println(a);
+                }
+            }
+        }
     }
 }
